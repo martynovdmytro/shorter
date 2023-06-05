bc992483f9662754ed01cdb3e13f57848a58fe85
Dmytro Martynov <dmytro_martynov@icloud.com>
2023-06-05 19:06:31 +0300

    queue driver config

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -3,7 +3,6 @@
 namespace App\Console;
 
 use App\Jobs\CheckUrlAvailability;
-use App\Services\LinkService;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
@@ -18,7 +17,6 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         $schedule->job(CheckUrlAvailability::class)
-            ->onQueue('check_url_availability')
             ->everyTwoMinutes()
             ->withoutOverlapping();
     }
